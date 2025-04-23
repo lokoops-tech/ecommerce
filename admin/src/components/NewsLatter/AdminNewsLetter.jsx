3a9de926c4ef640823145d275c802780d79b8da2
lokoops-tech <kosgeialex939@gmail.com>
2025-04-23 19:17:19 +0300

    Update AdminNewsLetter.jsx

--- admin/src/components/NewsLatter/AdminNewsLetter.jsx
+++ admin/src/components/NewsLatter/AdminNewsLetter.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import './AdminNewsletter.css';
+import './AdminNewsLetter.css';
 import { FiLoader } from 'react-icons/fi';
 import { Email_Subscribers } from '../../../Config';
 
