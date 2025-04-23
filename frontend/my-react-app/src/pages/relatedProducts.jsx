e8f9f1098bc54bdf663a76f055523e1497ef0007
lokoops-tech <kosgeialex939@gmail.com>
2025-04-23 19:59:19 +0300

    Update relatedProducts.jsx

--- frontend/my-react-app/src/pages/relatedProducts.jsx
+++ frontend/my-react-app/src/pages/relatedProducts.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect, useMemo } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './relatedProduct.css';
-import Item from '../components/Item/Item';
+import Item from '../components/item/Item';
 
 // Define the API endpoint as a constant
 const API_ENDPOINT = 'http://localhost:4000/product/allproducts';
