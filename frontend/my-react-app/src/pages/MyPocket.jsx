4a35bf952a313fccc32504a7ef455513ca3d825b
lokoops-tech <kosgeialex939@gmail.com>
2025-04-25 22:08:29 +0300

    Update MyPocket.jsx

--- frontend/my-react-app/src/pages/MyPocket.jsx
+++ frontend/my-react-app/src/pages/MyPocket.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom'; // Import Link
 import './MyPocket.css';
 
-const API_BASE_URL = "http://localhost:4000";
+const API_BASE_URL = "https://ecommerce-axdj.onrender.com";
 
 const MyPocket = () => {
   const [minPrice, setMinPrice] = useState('');
