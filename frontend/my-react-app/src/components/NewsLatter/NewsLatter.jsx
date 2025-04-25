c3e115c9ba354aaccf6f9bea50c4c6de2b649cbf
lokoops-tech <kosgeialex939@gmail.com>
2025-04-25 22:04:47 +0300

    Update NewsLatter.jsx

--- frontend/my-react-app/src/components/NewsLatter/NewsLatter.jsx
+++ frontend/my-react-app/src/components/NewsLatter/NewsLatter.jsx
@@ -3,7 +3,7 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import './NewsLatter.css';
 
-const API_BASE_URL = "http://localhost:4000";
+const API_BASE_URL = "https://ecommerce-axdj.onrender.com";
 
 const ParticleEffect = () => {
   return (
